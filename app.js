6cf65e05257438059f4a512eb98cc51092e5f33c
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-25 19:03:00 +0600

    handel case plus and minus

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('I am in a separate file')
+function getInputNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-input');
+    const caseInputNumber = parseInt(caseInput.value);
+    if (isIncreasing) {
+        caseInput.value = caseInputNumber + 1;
+    } else if (caseInputNumber > 0) {
+        caseInput.value = caseInputNumber - 1;
+    }
+
+}
+
+// handel case plus btn 
+document.getElementById('case-plus').addEventListener('click', function () {
+    getInputNumber(true);
+
+})
+// handel case minus btn  
+document.getElementById('case-minus').addEventListener('click', function () {
+    getInputNumber(false);
+    /* 
+    const caseInput = document.getElementById('case-input');
+    const caseInputNumber = parseInt(caseInput.value) ;
+    caseInput.value = caseInputNumber - 1; 
+    */
+})
